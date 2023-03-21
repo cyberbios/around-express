226f1ca99ba54277adf793a705977b13c910791e
LevMeir <alexandreisabella@hotmail.com>
2023-03-21 20:05:48 +0200

    Updated code after review

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
 const { PORT = 3000 } = process.env;
 
-const userRouter = require("./routes/users");
-const cardRouter = require("./routes/cards");
+const userRouter = require('./routes/users');
+const cardRouter = require('./routes/cards');
 
 app.use(userRouter);
 app.use(cardRouter);
 
 app.use((req, res) => {
-  res.status(404).send({ message: "Request resource not found" });
+  res.status(404).send({ message: 'Request resource not found' });
 });
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+  // console.log(`App listening on port ${PORT}`);
 });
--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,27 @@
-const router = require("express").Router();
-const path = require("path");
-const fs = require("fs").promises;
+const router = require('express').Router();
+const path = require('path');
+const fs = require('fs').promises;
 
-const dataPath = path.join(__dirname, "../data/users.json");
+const dataPath = path.join(__dirname, '../data/users.json');
 
-router.get("/users", (req, res) => {
-  fs.readFile(dataPath, { encoding: "utf-8" }).then((data) => res.send(data));
-});
-
-router.get("/users/:id", (req, res) => {
+router.get('/users/:id', (req, res) => {
+  const { id } = req.params;
   // read users fs promise
-  fs.readFile(dataPath, { encoding: "utf-8" })
+  fs.readFile(dataPath, { encoding: 'utf-8' })
     .then((data) => {
       const { id } = req.params;
 
       // find user by id
-      const user = JSON.parse(data).find((user) => user.id === id);
+      const user = JSON.parse(data).find((u) => u.id === id);
 
       if (user) {
         res.send(user);
       } else {
-        res.status(404).send({ message: "User ID not found" }); // user does not exist
+        res.status(404).send({ message: 'User ID not found' }); // user does not exist
       }
     })
-    .catch((err) => {
-      console.log("err =>", err);
-      res.status(500).send({ message: "We have encountered an error" }); // response server error
+    .catch(() => {
+      res.status(500).send({ message: 'We have encountered an error' }); // response server error
     });
 });
 
